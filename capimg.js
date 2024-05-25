02a093cc220d51696c745331bbdaa3ab430be8aa
janiofabio <35180272+janiofabio@users.noreply.github.com>
2024-05-25 09:54:12 -0300

    Update capimg.js

--- capimg.js
+++ capimg.js
@@ -2,13 +2,13 @@
 document.addEventListener('DOMContentLoaded', () => {
     const captureBtn = document.querySelector(".capture-btn");
     const snapBtn = document.querySelector(".snap-btn");
-    const video = document.querySelector("#video");
-    const canvas = document.createElement("canvas");
-    const capturedImage = document.querySelector(".captured-image");
-
-    let imageBase64 = '';
+    let video, canvas, capturedImage;
 
     captureBtn.addEventListener('click', async () => {
+        video = document.querySelector("#video");
+        canvas = document.querySelector("#canvas");
+        capturedImage = document.querySelector(".captured-image");
+
         try {
             const stream = await navigator.mediaDevices.getUserMedia({
                 video: {
@@ -16,9 +16,11 @@ document.addEventListener('DOMContentLoaded', () => {
                     focusMode: "continuous"
                 }
             });
-            video.style.display = 'block';
-            snapBtn.style.display = 'block';
-            video.srcObject = stream;
+            if (video && snapBtn) {
+                video.style.display = 'block';
+                snapBtn.style.display = 'block';
+                video.srcObject = stream;
+            }
         } catch (err) {
             console.error("Erro ao acessar a câmera traseira:", err);
             alert("Erro ao acessar a câmera traseira: " + err.message);
@@ -26,15 +28,21 @@ document.addEventListener('DOMContentLoaded', () => {
     });
 
     snapBtn.addEventListener('click', () => {
-        const context = canvas.getContext('2d');
-        canvas.width = video.videoWidth;
-        canvas.height = video.videoHeight;
-        context.drawImage(video, 0, 0, video.videoWidth, video.videoHeight);
-        video.style.display = 'none';
-        snapBtn.style.display = 'none';
-        capturedImage.style.display = 'block';
-        capturedImage.src = canvas.toDataURL('image/jpeg');
-        imageBase64 = capturedImage.src.split(',')[1];
-        canvas.style.display = 'none';
+        video = document.querySelector("#video");
+        canvas = document.querySelector("#canvas");
+        capturedImage = document.querySelector(".captured-image");
+
+        if (canvas && video && capturedImage) {
+            const context = canvas.getContext('2d');
+            canvas.width = video.videoWidth;
+            canvas.height = video.videoHeight;
+            context.drawImage(video, 0, 0, video.videoWidth, video.videoHeight);
+            video.style.display = 'none';
+            snapBtn.style.display = 'none';
+            capturedImage.style.display = 'block';
+            capturedImage.src = canvas.toDataURL('image/jpeg');
+            imageBase64 = capturedImage.src.split(',')[1];
+            canvas.style.display = 'none';
+        }
     });
 });
